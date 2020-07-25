8a0f7e096656c356e2573c4fb1f99313566e1c1b
hoangpt2991 <59075275+hoangpt2991@users.noreply.github.com>
2020-07-25 23:36:47 +0700

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,27 +1,23 @@
 pipeline {
-         agent any
-         stages {
-                 stage('Checking SCM folder') {
-                       steps {
-                           echo 'Hi, GeekFlare. Starting to build the App.'
-                           sh '''
-                           rm -rf *
-                           '''
-                       }
-                 }
-                 stage('Test') {
-                       steps {
-                          input('Do you want to proceed?')
-                       }
-                 }
-                 stage('Deploy') {
-                 
-                 stage('Prod') {
-                       steps {
-                                  echo "App is Prod Ready"
-                                }
-
-              }
-}
-}
+    agent any 
+    stages {
+        stage('Checking repo') { 
+            steps {
+                echo "1234"
+                sh '''
+                rm -rf * 
+                '''
+            }
+        }
+        stage('Test') { 
+            steps {
+                // 
+            }
+        }
+        stage('Deploy') { 
+            steps {
+                // 
+            }
+        }
+    }
 }
